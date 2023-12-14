9a185b29b0366d748481947878f99c256574f512
sft <minbo2002@naver.com>
2023-12-14 15:57:16 +0900

    [feat] 첫 환경 셋팅

---
+++ src/main/java/com/example/paekaempay/PaekaemPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.paekaempay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaekaemPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaekaemPayApplication.class, args);
+    }
+
+}
